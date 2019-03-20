934d1a3f69e0d229404f90f9219b40299e5bdacb
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-03-20 15:58:11 -0400

    Shoppers Mall 1.0  /social_links.vue

--- social_links.vue
+++ social_links.vue
@@ -3,6 +3,7 @@
         <a href="https://www.facebook.com/ShoppersMall" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i><p style="display:none">Facebook</p></a>
         <a href="https://www.twitter.com/ShoppersMall1" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i><p style="display:none">Twitter</p></a>
         <a href="https://www.instagram.com/Shoppersmall1" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i><p style="display:none">Instagram</p></a>
+        <a href="https://www.pinterest.ca/ShoppersMall" target="_blank"><i class="fa fa-pinterest" aria-hidden="true"></i><p style="display:none">Facebook</p></a>
     </div>
     
 </template>
