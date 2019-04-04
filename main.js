0c55b9eaf37f90838b31ae8fcc58ec77d19ff0aa
alex@mobilefringe.com @ CodeCloud <alex@mobilefringe.com @ CodeCloud>
2019-04-04 11:19:41 -0400

    Shoppers Mall 1.0  /main.js

--- main.js
+++ main.js
@@ -82,7 +82,7 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'datastore', 'vu
             return {
                 dataLoaded: false,
                 show_mobile_menu: false,
-                property_logo: "//codecloud.cdn.speedyrails.net/sites/5c9282796e6f645b4d330000/image/png/1554390402001/1116960616920929.2H7zV1gI4pZYWxo6NOtl_height640.png",
+                property_logo: "//codecloud.cdn.speedyrails.net/sites/5c9282796e6f645b4d330000/image/png/1554391086802/ShoppersMall-01-White-01.png",
                 menu_items: MenuItems,
                 search_result : null,
                 suggestionAttribute: 'name',
@@ -96,7 +96,7 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'datastore', 'vu
         created() {
             this.loadData().then(response => {
                 this.locale = "en-ca";
-                this.property.default_logo_url = "//codecloud.cdn.speedyrails.net/sites/5c9282796e6f645b4d330000/image/png/1554390402001/1116960616920929.2H7zV1gI4pZYWxo6NOtl_height640.png";
+                this.property.default_logo_url = "//codecloud.cdn.speedyrails.net/sites/5c9282796e6f645b4d330000/image/png/1554391086802/ShoppersMall-01-White-01.png";
                 this.dataLoaded = true;
                 
             });
