b12305d98b1933f227f4cb26695acbc491e0bc96
alex@mobilefringe.com @ CodeCloud <alex@mobilefringe.com @ CodeCloud>
2019-04-04 11:43:04 -0400

    Shoppers Mall 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -192,11 +192,11 @@
                         // else {
                         //     value.mobile_order = key + 1;
                         // }
-                        if(key == 1) {
+                        if(key == 2) {
                             value.mobile_order = 6;
                         }
                         else if(key == 6) {
-                            value.mobile_order = 1;
+                            value.mobile_order = 2;
                         }
                         else {
                             value.mobile_order = key;
