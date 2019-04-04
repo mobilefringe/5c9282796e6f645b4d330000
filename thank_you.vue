b1c6f85e0cd79e30625f313a56287c49b252de0a
alex@mobilefringe.com @ CodeCloud <alex@mobilefringe.com @ CodeCloud>
2019-04-04 16:13:45 -0400

    Shoppers Mall 1.0  /thank_you.vue

---
+++ thank_you.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="page_container" id="thank_you_container">
+        <div v-if="pageBanner" class="page_header" v-bind:style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">
+			<!--http://via.placeholder.com/1920x300-->
+			<div class="site_container">
+				<div class="header_content">
+					<h1>Newsletter</h1>
+				</div>
+			</div>
+		</div>
+        <div class="row site_container">
+            Your subscription has been confirmed. You've been added to our list and will hear from us soon.
+        </div>
+    </div>
+</template>
+<style>
+    #thank_you_container div{
+        padding: 20px;
+    }
+</style>
+<script>
+    define(["Vue", "vuex"], function(Vue,Vuex) {
+        return Vue.component("thank-you-component", {
+            template: template, // the variable template will be injected
+            computed: {
+                ...Vuex.mapGetters([
+                    'property',
+                    'timezone',
+                    'findRepoByName'
+                ]),
+                pageBanner(){
+                    var temp_repo = null;
+                    var pageBanner = null;
+                    //Add custom banners for indivial pages 
+                    temp_repo = this.findRepoByName('Pages Banner');
+                    
+                    if(temp_repo && temp_repo.images) {
+                        pageBanner = temp_repo.images[0];
+                    }
+                    else {
+                        pageBanner = {};
+                        pageBanner.image_url = "";
+                    }
+                    return pageBanner;
+                }
+            },
+            
+        });
+    });
+</script>
