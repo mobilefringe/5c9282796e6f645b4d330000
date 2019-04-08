d75e28a6add3e36703acc84ddef679ed39de10e7
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-04-08 13:41:55 -0400

    Shoppers Mall 1.0  /map.vue

--- map.vue
+++ map.vue
@@ -1,6 +1,6 @@
 <template>
 	<div v-if="dataloaded">
-	    <div class="page_header" v-if="pageBanner" :style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">
+	    <div v-if="pageBanner" class="page_header" v-bind:style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">
 			<!--http://via.placeholder.com/1920x300-->
 			<div class="site_container">
 				<div class="header_content">
