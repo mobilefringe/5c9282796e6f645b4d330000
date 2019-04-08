1b317f9413c2d2aac172ce4312cc66532649facf
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-04-08 13:37:07 -0400

    Shoppers Mall 1.0  /map.vue

--- map.vue
+++ map.vue
@@ -66,6 +66,7 @@
                         this.pageBanner = {};
                         this.pageBanner.image_url = "";
                     }
+                    console.log(this.pageBanner)
                 });
             },
             watch: {
