53983f50581b61ff249acd9a2095f1b828ad6616
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-04-11 17:54:46 -0400

    Shoppers Mall 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -254,6 +254,7 @@
                     try {
                         // avoid making LOAD_META_DATA call for now as it will cause the entire Promise.all to fail since no meta data is set up.
                         let results = await Promise.all([this.$store.dispatch("getData","promotions"), this.$store.dispatch("getData", "jobs"),this.$store.dispatch("getData", "repos")]);
+                        console.log(results)
                     } catch (e) {
                         console.log("Error loading data: " + e.message);
                     }
