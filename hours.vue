26f7305218acd38ac35c06fa92323af83c632f70
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-04-11 16:18:13 -0400

    Shoppers Mall 1.0  /hours.vue

--- hours.vue
+++ hours.vue
@@ -16,11 +16,11 @@
                         <h3 class="hours_heading text-left" >{{$t("hours_page.reg_hours")}}</h3>
                         <div id="hours_container" class="hours_container">
                             <div class="hours_div" v-for="hour in hours">
-                                <div class="col-sm-3">
-                                    <p>{{hour.day_of_week | moment("dddd", timezone)}}: </p>
+                                <div class="col-sm-6">
+                                    <p class="hours_days">{{hour.day_of_week | moment("dddd", timezone)}}: </p>
                                 </div>
-                                <div class="col-sm-3">
-                                    <p>{{hour.open_time | moment("h:mm A", timezone)}} - {{hour.close_time | moment("h:mm A", timezone)}}</p>
+                                <div class="col-sm-6">
+                                    <p class="hours_days">{{hour.open_time | moment("h:mm A", timezone)}} - {{hour.close_time | moment("h:mm A", timezone)}}</p>
                                 </div>
                             </div>
                         </div>
