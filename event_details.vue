6e3bff02c8c8dbc175835b45333a4cb4c4a5bdc6
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-04-12 16:56:14 -0400

    Shoppers Mall 1.0  /event_details.vue

--- event_details.vue
+++ event_details.vue
@@ -23,7 +23,7 @@
 						<p class="promo_div_date pull-left" v-else>{{currentEvent.start_date | moment("MMM D", timezone)}}</p>
 					</div>
 					<social-sharing :url="$root.shareURL('events',currentEvent.slug)" :title="currentEvent.title" :description="currentEvent.body" :quote="_.truncate(currentEvent.description, {'length': 99})" :twitter-user="$root.twitter_user" :media="currentEvent.image_url" inline-template >
-						<div class="blog-social-share" style="margin: 0 auto 15px;">
+						<div class="blog-social-share" style="margin: 0">
 							<div class="social_share">
 								<network network="facebook">
 									<i class="fa fa-facebook social_icons" aria-hidden="true"></i>
