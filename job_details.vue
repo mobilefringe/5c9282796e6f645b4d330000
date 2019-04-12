91cce869d83aea7fe389e692f5b903240d8d0bd1
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-04-12 16:56:28 -0400

    Shoppers Mall 1.0  /job_details.vue

--- job_details.vue
+++ job_details.vue
@@ -52,7 +52,7 @@
 						<p class="promo_div_date pull-left">{{currentJob.start_date | moment("MMM D", timezone)}} - {{currentJob.end_date | moment("MMM D", timezone)}}</p>
 					</div>
 					<social-sharing :url="$root.shareURL('jobs',currentJob.slug)" :title="currentJob.title" :description="currentJob.body" :quote="_.truncate(currentJob.description, {'length': 99})" :twitter-user="$root.twitter_user" :media="currentJob.image_url" inline-template >
-						<div class="blog-social-share" style="margin: 0 auto 15px;">
+						<div class="blog-social-share" style="margin: 0">
 							<div class="social_share">
 								<network network="facebook">
 									<i class="fa fa-facebook social_icons" aria-hidden="true"></i>
