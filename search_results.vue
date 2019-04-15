74976b6fd64f859a69d91678927375d47b5c130a
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-04-15 14:33:48 -0400

    Shoppers Mall 1.0  /search_results.vue

--- search_results.vue
+++ search_results.vue
@@ -16,6 +16,9 @@
         			<div v-for="(result,index) in searchResults" :key="index">
                         <div class="row result_container_row">
                             <div v-if="result.is_store" class="col-sm-3 store_details_image center-block">
+                                <div v-if="result.hasOwnProperty('promo_image_url_abs')">
+                                    <img :src="result.promo_image_url_abs"
+                                </div>
                                 <div v-if="checkResultImage(result)">
                                     <div class="no_logo">
                                         <img src="//codecloud.cdn.speedyrails.net/sites/5b88438d6e6f641e8d3c0000/image/png/1536092029690/transparent_logo.png">
