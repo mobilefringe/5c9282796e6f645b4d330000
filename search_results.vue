10990aa383284772f6766cf6873f2858aa8b2710
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-04-15 12:00:18 -0400

    Shoppers Mall 1.0  /search_results.vue

--- search_results.vue
+++ search_results.vue
@@ -31,7 +31,7 @@
                                 </div>
                             </div>
                             <div v-else class="col-sm-3 store_details_image center-block">
-                                <img class="result_logo" :src="result.image_url || result.store.image_url"/>   
+                                <img class="result_logo" :src="result.image_url"/>   
                             </div>
                             <div class="col-sm-9 search_result_content">
                                 <h3>{{result.name}}</h3>
