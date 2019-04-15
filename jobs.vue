0ea14f39377fc96e2950de85e6f90d1eae0af9ee
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-04-15 11:30:07 -0400

    Shoppers Mall 1.0  /jobs.vue

--- jobs.vue
+++ jobs.vue
@@ -135,9 +135,9 @@
                             value.description_short_2 = _.truncate(value.description_2, {
                                 'length': 150
                             });
-                            // if (value.store != null && value.store != undefined && _.includes(value.store.store_front_url_abs, 'missing')) {
-                            //     value.store.store_front_url_abs = vm.property.default_logo_url;
-                            // }
+                            if (value.store != null && value.store != undefined && _.includes(value.store.store_front_url_abs, 'missing')) {
+                                value.store.store_front_url_abs = vm.property.default_logo_url;
+                            }
                             // else if (value.store == null || value.store == undefined) {
                             //     value.store = {};
                             //     value.store.store_front_url_abs =  vm.property.default_logo_url;
