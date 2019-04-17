f6ac461aba743fac86d61c88557f3c7f706d6295
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-17 15:23:12 -0400

    Shoppers Mall 1.0  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -160,7 +160,7 @@
                             temp_promo.push(value);
                         }
                     });
-                    temp_promo = _.sortBy(temp_promo, ['created_at', 'start_date']).reverse();
+                    temp_promo = _.sortBy(temp_promo, ['end_date']);
                     return temp_promo;
                 },
             },
