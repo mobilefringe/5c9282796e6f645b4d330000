30c07beeb59814df340313688dd00e2f939fd8c6
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-05-02 13:41:36 -0400

    Shoppers Mall 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -179,7 +179,7 @@
                     
                     var storeHours = [];
                     var vm = this;
-                    console.log("this.currentStore.store_hours", this.currentStore.store_hours)
+                    
                     _.forEach(this.currentStore.store_hours, function (value, key) {
                         var hour = vm.findHourById(value);
                         if(hour.day_of_week === 0){
@@ -191,6 +191,7 @@
                         storeHours.push(hour);
                     });
                     this.storeHours = _.sortBy(storeHours, [function(o) { return o.order; }]);
+                    console.log("this.storeHours", this.storeHours)
                 }
             },
             
