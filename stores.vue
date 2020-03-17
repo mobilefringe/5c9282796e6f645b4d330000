1af136df2ec5ddfdc43d9b978df4cabba3d63546
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2020-03-17 18:19:40 -0400

    Shoppers Mall 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -81,7 +81,7 @@
                         this.pageBanner = {};
                         this.pageBanner.image_url = "";
                     }
-                    
+                    console.log(response[1].data)
                     this.currentPage = response[1].data
                 });
             },
